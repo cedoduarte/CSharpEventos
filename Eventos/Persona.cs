2be9a2ae75a69d335d8015e77d4a522e2294d594
cedoduarte <carlosduarte.1@hotmail.com>
2015-11-29 19:59:16 -0600

    first commit

---
+++ Eventos/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventos
+{
+    public class Persona
+    {
+        public void PateaPelota()
+        {
+            PelotaPateada(this, null);
+        }
+
+        public event EventHandler PelotaPateada;
+    }
+}
---
+++ Eventos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var juan = new Persona();
+            juan.PelotaPateada += new EventHandler(PelotaPateada);
+            juan.PateaPelota();
+            Console.Read();
+        }
+
+        public static void PelotaPateada(object sender, EventArgs e)
+        {
+            Console.WriteLine("La pelota ha sido pateada");
+        }
+    }
+}
